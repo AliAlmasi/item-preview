5642260918d06bc4296d7e5879f475106e20858c
Ali Almasi <A710almasi@gmail.com>
2022-07-17 01:43:31 +0430

    styles added

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 console.log("It's Working. . .");
+
+function visitPage() {
+  if (confirm("Are you sure to redirect to Jonas Schmedtmann's Pen?")) {
+  window.location = "https://codepen.io/jonasschmedtmann/pen/48f20ea036df9afc09978b07eca226b8";
+} else {
+  return null;
+}
+}
